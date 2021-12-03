2c8a1714795fd9b05349d755f5cc4d5a20a0070a
Daniell Mesquita <daniellmesquita@protonmail.com>
2021-12-03 17:16:30 -0300

    Improve previous commit

--- mlq-parser.sh
+++ mlq-parser.sh
@@ -2,13 +2,13 @@
 
 if [ "$1" = "--write" ];then
    text_to_insert=$(echo "$2" | grep -o '"...\+"' | tr -d '"')
-   echo "it have to work: $2"
-   echo "the previous did work. But: $text_to_insert"
+#   echo "it have to work: $2"
+#   echo "the previous did work. But: $text_to_insert"
    tagtree=$(echo "$2" | grep -o '#[[:alpha:]]\+*' | awk 'NF { print "\""$0"\""}' | tr "\n" " " | tr '"' "'" | tr -d '#')
 #   sh ./mlq-parser_worker.sh --tree
 #   sh ./mlq-parser_worker.sh --insert
    tagtreesingle=$(echo "$tagtree" | tr -d "'")
-   echo "mitigate ok: $3"
-   echo "mitigate all: $tagtreesingle, $text_to_insert"
+#   echo "mitigate ok: $3"
+#   echo "mitigate all: $tagtreesingle, $text_to_insert"
    bash /usr/lib/mlq/mlq-parser_worker.sh --singletag $tagtreesingle "$text_to_insert" "$3"
 fi
