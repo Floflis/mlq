8d127249219f7b80b3b2e6e317399349f3a6248b
Daniell Mesquita <daniellmesquita@protonmail.com>
2021-12-02 22:33:39 -0300

    fix any issue when using variables - full support

--- mlq-parser.sh
+++ mlq-parser.sh
@@ -2,9 +2,11 @@
 
 if [ "$1" = "--write" ];then
    text_to_insert=$(echo "$2" | grep -o '"[[:alpha:]]\+*"' | tr -d '"')
+#   echo "it have to work: $2"
+#   echo "the previous did work. But: $text_to_insert"
    tagtree=$(echo "$2" | grep -o '#[[:alpha:]]\+*' | awk 'NF { print "\""$0"\""}' | tr "\n" " " | tr '"' "'" | tr -d '#')
 #   sh ./mlq-parser_worker.sh --tree
 #   sh ./mlq-parser_worker.sh --insert
    tagtreesingle=$(echo "$tagtree" | tr -d "'")
-   bash /usr/lib/mlq/mlq-parser_worker.sh --singletag $tagtreesingle $text_to_insert $3
+   bash /usr/lib/mlq/mlq-parser_worker.sh --singletag $tagtreesingle "$text_to_insert" "$3"
 fi
